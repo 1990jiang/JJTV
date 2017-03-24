31672724db2ea2983bcfe83dd073a29d9a8dbc8d
Tmac <562752212@qq.com>
2017-03-24 19:36:05 +0800

    再次提交，试一试，这个版本不用看的

--- JJTv/Classes/Home/Controller/HomeViewController.swift
+++ JJTv/Classes/Home/Controller/HomeViewController.swift
@@ -35,7 +35,12 @@ extension HomeViewController {
     
     private func setupNavigationBar(){
         
+        // 1.左侧logoItem
+        let logoImage = UIImage(named: "home-logo")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: logoImage, style: .plain, target: nil, action: nil)
         
+        // 2.设置右侧收藏的item
+
         
         
     }
